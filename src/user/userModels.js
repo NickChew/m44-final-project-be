1d055b192bc648fe1c0af589cff3370939bc79e2
hsnadrs <hsnadrs@yahoo.com>
2023-02-08 15:43:19 +0000

    test

--- src/user/userModels.js
+++ src/user/userModels.js
@@ -1,27 +1,29 @@
-// // change to sequalize
-// // const mongoose = require("mongoose");
-//this is Hasan
-// const userSchema = new mongoose.Schema ({
-//   username: {
-//     type: String,
-//     required: true,
-//     unique: true
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//     match: /^\S+@\S+$/
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//     unique: false,
-//     match: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
-//   }
-// });
 
-// // change to sequalize
-// // const User = mongoose.model("user", userSchema);
+// user model is the same like what we got from the lessos
+// no need for ID as it is AUTOMATICALLY CREATED by mySQL 
+// we will get userID when needed via userID= Users.findOne (name : requestAnimationFrame.body.name););
+const mongoose = require("mongoose");
 
-// module.exports = User;
+const userSchema = new mongoose.Schema ({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^\S+@\S+$/
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: false,
+        match: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
+    }
+})
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
