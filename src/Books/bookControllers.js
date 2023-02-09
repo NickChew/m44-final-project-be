a1edb43b156c9cfa7c644e28065745dc78c1d392
hsnadrs <119332408+hsnadrs@users.noreply.github.com>
2023-02-09 11:28:23 +0000

    Update bookControllers.js
    this code is not tested on Database and IDs name of parameters has to be change to be similar to TABLES and FORMS  variable
    just tested on VS for syntax errors.
    
    As suggested by Mr Mark yesterday as we deal with 4 tables
    each create or insert row  must be done to two tables e.g. book and user_books tables
    
    same thing for wish list insertion into two tables.
    
    for listing all books belong to a user we use to find() first to return all bookID for the userID, 
    second find ()  inside the  loop to save all book's detail into the RETURN Response

--- src/Books/bookControllers.js
+++ src/Books/bookControllers.js
@@ -1,4 +1,4 @@
-const users = require("./users");
+const users = require("../user/userModels");
 const booksModel = require("./booksModel");
 const users2booksModel = require("./users2booksModel");
 const wishlistModel = require("./wishlistModel");
@@ -7,7 +7,11 @@ const wishlistModel = require("./wishlistModel");
 exports.addBooks = async (request, response) => {
     console.log(request);
     try {
-        const newBooks = await booksModel.create(request.body);
+        //request.body should have userID + all book information + user_books extra info
+        //first add books data to books table
+        const newBooks = await booksModel.create(request.body.bookID, request.body.title, request.body.authors, request.body.imageURL);
+        //second add UserID and bookID into user_books table also add in extra data into this table 
+        const newUsers_Books = await users2booksModel.create(request.body.UserID, request.body.bookID);
         response.status(200).send({ booksModel: newBooks });
     } catch (error) {
         console.log(error);
@@ -19,7 +23,13 @@ exports.addBooks = async (request, response) => {
 exports.addWishBooks = async (request, response) => {
     console.log(request);
     try {
-        const newWishBooks = await wishlistModel.create(request.body);
+        //const newWishBooks = await wishlistModel.create(request.body);
+         //request.body should have userID + bookID + user_wish extra info
+        //first add books data to books table
+        const newBooks = await booksModel.create(request.body.bookID, request.body.title, request.body.authors, request.body.imageURL);
+        
+        //second add UserID and bookID into user_books table also add in extra data into this table 
+        const newWishBooks = await wishlistModel.create(request.body.userID, request.body.bookID);
         response.status(200).send({ wishlistModel: newWishBooks });
     } catch (error) {
         console.log(error);
@@ -30,8 +40,17 @@ exports.addWishBooks = async (request, response) => {
 //list - change too list users books in database and or list wishList books - to be done!
 exports.listBooks = async (request, response) => {
     try {
-        const books = await booksModel.find({});
-        response.status(218).send({ booksModel: books });
+        //request will have userID, 
+        //store all this user books info including IDs in JSON then loop through
+        const userBooks = await users2booksModel.find({where: {ID: UserID}});
+        //now loop through user booksID to show all information of this books
+        const UserBooksDetails ={};// store this user books details into object
+        for (let i=0; i< userBooks.length(); i++){
+        const temp = await booksModel.find({where: {ID: userBooks[0].ID}});// find one book each iteration 
+            UserBooksDetails.push(temp);// add it to the returned books object
+        }
+            
+        response.status(218).send({ booksModel: UserBooksDetails });// return the books detail in JSON to be displaid
     } catch (error) {
         console.log(error);
         response.status(500).send({error: error.message});
@@ -53,7 +72,11 @@ exports.listBooks = async (request, response) => {
 //  
 exports.deleteBooks = async (request, response) => {
     try {
-        const deletedBooks = await booksModel.deleteOne({title: request.body.title});
+        //request will have userID + bookID, 
+        
+        //easily delete it from users_books ER table
+        const deletedBooks = await users2booksModel.deleteOne({userID: request.body.title, userID : request.body.bookID});
+            
         if (deletedBooks.deletedCount > 0) {
             response.status(200).send({ booksModel: deletedBooks });
         }
@@ -68,7 +91,11 @@ exports.deleteBooks = async (request, response) => {
 //following should delete book from wishlist table/databse - to be done!
 exports.deleteWishBooks = async (request, response) => {
     try {
-        const deletedWishBooks = await wishlistModel.deleteOne({title: request.body.title});
+        //request will have userID + bookID, 
+        
+        //easily delete it from users_wishBooks ER table
+        const deletedBooks = await wishlistModel.deleteOne({userID: request.body.title, userID : request.body.bookID});
+            
         if (deletedWishBooks.deletedCount > 0) {
             response.status(200).send({ wishlistModel: deletedWishBooks });
         }
