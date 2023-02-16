4cc4224746a673efe08bc8230e106a90dfa17638
NickChew <118730370+NickChew@users.noreply.github.com>
2023-02-16 10:01:46 +0000

    added token check to bookroutes

--- src/Books/bookRoutes.js
+++ src/Books/bookRoutes.js
@@ -6,12 +6,12 @@ const {tokenCheck} = require("../middleware"); //because index.js otherwise path
 
 const bookRouter = Router();
 
-bookRouter.post("/addBook", addBooks);
-bookRouter.post("/addWishBook", addWishBooks);
+bookRouter.post("/addBook", tokenCheck, addBooks);
+bookRouter.post("/addWishBook", tokenCheck, addWishBooks);
 bookRouter.get("/listBooks", tokenCheck, listBooks);
 bookRouter.get("/listWishBooks", tokenCheck, listWishBooks);
-bookRouter.delete("/deleteBook", deleteBooks);
-bookRouter.delete("/deleteWishBook", deleteWishBooks)
+bookRouter.delete("/deleteBook", tokenCheck, deleteBooks);
+bookRouter.delete("/deleteWishBook", tokenCheck, deleteWishBooks);
 
 module.exports = bookRouter;
 
